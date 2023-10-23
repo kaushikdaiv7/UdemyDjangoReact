cffd78dac3317538360444bbc7e06ccf9dc179ac
kaushikdaiv7 <kaushik777d@gmail.com>
2023-10-22 18:55:07 -0700

    Created Model, Migrations, Views, Urls

--- demo/models.py
+++ demo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=36, blank=False, unique=True)
+    description = models.TextField(max_length=256, blank=True)
+
+    price = models.DecimalField(default=0, max_digits=10, decimal_places=2)
+
+    isPublished = models.BooleanField(default=False)
+    published = models.DateField(blank=True, null=True, default=None)
+
+    cover = models.ImageField(upload_to='covers/', blank=True)
